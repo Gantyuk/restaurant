f6b57bd9a26ebe6f6d08c5c1da7cdf37854c714c
goodlife1 <vasyamindresku@mail.ru>
2017-03-07 18:31:00 +0200

    Module Admin

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Caffeinated\Modules\ModulesServiceProvider::class,
 
     ],
 
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Module' => Caffeinated\Modules\Facades\Module::class,
+
 
     ],
 
